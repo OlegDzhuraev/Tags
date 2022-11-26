e082ca8525ece29d205d15e11c990a1f7f185966
Oleg Dzhuraev <godlikeaurora@gmail.com>
2022-11-26 16:00:05 +0300

    Example was extended

--- Example/Sources/ExampleStartTagsAdd.cs
+++ Example/Sources/ExampleStartTagsAdd.cs
@@ -17,22 +17,42 @@ namespace Tags.Example
 
 		IEnumerator ExampleAction()
 		{
-			Debug.Log("Example adding and removing tags.");
+			LogHeading("Add tag example:");
 			gameObject.AddTag(ExampleTag.DisableMovement);
 
-			yield return new WaitForSeconds(0.5f);
+			yield return new WaitForSeconds(1f);
 			
+			LogHeading("Add another tag:");
 			gameObject.AddTag(ExampleTag.IsPoisoned);
+
+			Debug.Log($"Checking that tag {ExampleTag.IsPoisoned} was really added...");
+
+			if (gameObject.HaveTag(ExampleTag.IsPoisoned))
+				Debug.Log($"Object has tag {ExampleTag.IsPoisoned}!");
+
+			yield return new WaitForSeconds(1f);
 			
-			yield return new WaitForSeconds(0.5f);
+			LogHeading("Example stacking IsPoisoned tags:");
+			gameObject.AddTag(ExampleTag.IsPoisoned);
+			Debug.Log($"Counting amount of {ExampleTag.IsPoisoned}...");
+			var tagsCount = gameObject.CountTags(ExampleTag.IsPoisoned);
+			Debug.Log($"Object have {tagsCount} {ExampleTag.IsPoisoned} tags");
 			
+			yield return new WaitForSeconds(1f);
+			
+			LogHeading("Tag remove example:");
 			gameObject.RemoveTag(ExampleTag.DisableMovement);
 			
-			yield return new WaitForSeconds(0.5f);
+			yield return new WaitForSeconds(1f);
 			
-			Debug.Log("Example stacking IsPoisoned tags (now should be 2 on object).");
-			gameObject.AddTag(ExampleTag.IsPoisoned);
+			LogHeading("Checking that 2 tags added in the same time:");
+			gameObject.AddTag(ExampleTag.AlternativeAttackEnabled);
+			
+			var isTwoTags = gameObject.HaveAllTags(ExampleTag.IsPoisoned, ExampleTag.AlternativeAttackEnabled);
+			Debug.Log($"Is GameObject have {ExampleTag.IsPoisoned} and {ExampleTag.AlternativeAttackEnabled}: {isTwoTags}");
 		}
+
+		void LogHeading(string text) => Debug.Log($"<color=yellow><b>{text}</b></color>");
 
 		private void OnTagWasAdded(IntTag intTag)
 		{
